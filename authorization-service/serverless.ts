014debf4fa58e982b4402ac0e944ad7104362c8c
Volodymyr Prodyus <Volodymyr_Prodyus@epam.com>
2023-11-10 11:30:31 +0200

    feat(task_7): add custom prop

--- authorization-service/serverless.ts
+++ authorization-service/serverless.ts
@@ -20,6 +20,18 @@ const serverlessConfiguration: AWS = {
     // import the function via paths
     functions: { basicAuthorizer },
     package: { individually: true },
+    custom: {
+        esbuild: {
+            bundle: true,
+            minify: false,
+            sourcemap: true,
+            exclude: ['aws-sdk'],
+            target: 'node18',
+            define: { 'require.resolve': undefined },
+            platform: 'node',
+            concurrency: 10,
+        },
+    },
 };
 
 module.exports = serverlessConfiguration;
